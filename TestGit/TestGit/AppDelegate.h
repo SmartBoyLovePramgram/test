1867ad356cf7968590b6a006e150858a627e86a0
lixiaoqiang <lixiaoqiang@yixia.com>
2017-12-03 12:47:32 +0800

    测试git

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by lixiaoqiang on 2017/12/3.
+//  Copyright © 2017年 lixiaoqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
